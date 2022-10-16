c634e04ca09977ecd96cb887cfdebe93de1d60dc
JG.JI <junggu.ji.dev@gmail.com>
2022-10-16 15:47:46 +0900

    Feat : Member 생성

---
+++ src/main/java/com/jgji/sokdak/domain/member/Email.java
@@ -0,0 +1,24 @@
+package com.jgji.sokdak.domain.member;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class Email {
+
+    @jakarta.validation.constraints.Email
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Builder
+    public Email(@jakarta.validation.constraints.Email String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/jgji/sokdak/domain/member/Member.java
@@ -0,0 +1,41 @@
+package com.jgji.sokdak.domain.member;
+
+import com.jgji.sokdak.global.model.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member")
+@Entity
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private long id;
+
+    @Embedded
+    private Email email;
+
+    @Column(name = "nickname", nullable = false, unique = true)
+    private String nickname;
+
+    @Embedded
+    private Password password;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Builder
+    public Member(Email email, String nickname, Password password, String profileImage) {
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+        this.profileImage = profileImage;
+    }
+}
