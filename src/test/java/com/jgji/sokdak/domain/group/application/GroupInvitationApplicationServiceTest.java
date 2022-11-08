1f4f7ff5dda120344be5d8b6039bee1e2c1b8938
JG.JI <junggu.ji.dev@gmail.com>
2022-11-08 23:07:06 +0900

    Test : 초대코드 생성 테스트코드 작성

---
+++ src/test/java/com/jgji/sokdak/domain/group/application/GroupInvitationApplicationServiceTest.java
@@ -0,0 +1,69 @@
+package com.jgji.sokdak.domain.group.application;
+
+import com.jgji.sokdak.domain.group.domain.GroupInvitation;
+import com.jgji.sokdak.domain.group.domain.GroupInvitationRepository;
+import com.jgji.sokdak.domain.group.exception.AlreadyExistException;
+import com.jgji.sokdak.domain.member.domain.Member;
+import com.jgji.sokdak.domain.member.domain.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Slf4j
+@SpringBootTest
+class GroupInvitationApplicationServiceTest {
+
+    @Autowired
+    private GroupInvitationApplicationService groupInvitationApplicationService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private GroupFindService groupFindService;
+
+    @Autowired
+    private GroupInvitationRepository groupInvitationRepository;
+
+    private Member member;
+    @BeforeEach
+    void setUp() {
+        this.member = this.memberRepository.findById(1L).get();
+    }
+
+    @DisplayName(value = "초대코드 생성")
+    @Test
+    void generateCode() {
+        //given
+        //when
+        String generateCode = this.groupInvitationApplicationService.generateCode(member, 1L);
+        //then
+
+        assertThat(is(generateCode.length() > 0));
+        log.info("code ={}", generateCode);
+    }
+
+    @DisplayName(value = "이미 유효한 초대코드가 존재함")
+    @Test
+    void fail() {
+        //given
+        GroupInvitation groupInvitation = GroupInvitation.builder()
+                .code("QWERASDF")
+                .memberId(member.getId())
+                .groupId(1L)
+                .build();
+
+        this.groupInvitationRepository.save(groupInvitation);
+
+        //when
+        assertThrows(AlreadyExistException.class, () ->
+                this.groupInvitationApplicationService.generateCode(member, 1L));
+    }
+}
