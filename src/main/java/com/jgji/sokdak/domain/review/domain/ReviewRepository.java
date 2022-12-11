6fa6d184d15b04417444f7ac5c55c064390b8625
JG.JI <junggu.ji.dev@gmail.com>
2022-12-11 17:35:15 +0900

    Test

---
+++ src/main/java/com/jgji/sokdak/domain/review/domain/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.jgji.sokdak.domain.review.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
